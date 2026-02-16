5876c4fe6ed9fb6885ff8d8f21ea8ca82e99c149
Johann Li <me@qinka.pro>
2026-02-16 13:17:18 +0800

    更新协议

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
 // 该文件是 Shanan （山南西风） 项目的一部分。
 // src/lib.rs - 过程宏库主文件
 //
-// 本程序遵循 GNU Affero 通用公共许可证（AGPL）许可协议。
-// 本程序的发布旨在提供实用价值，但不作任何形式的担保，
-// 包括但不限于对适销性或特定用途适用性的默示担保。
-// 更多详情请参阅 GNU 通用公共许可证。
+// 本文件根据 Apache 许可证第 2.0 版（以下简称“许可证”）授权使用；
+// 除非遵守该许可证条款，否则您不得使用本文件。
+// 您可通过以下网址获取许可证副本：
+// http://www.apache.org/licenses/LICENSE-2.0
+// 除非适用法律要求或书面同意，根据本许可协议分发的软件均按“原样”提供，
+// 不附带任何形式的明示或暗示的保证或条件。
+// 有关许可权限与限制的具体条款，请参阅本许可协议。
 //
-// Copyright (C) 2026 Johann Li <me@qinka.pro>, ETVP
+// Copyright (C) 2026 Johann Li <me@qinka.pro>, Wareless Group
 
 use proc_macro::TokenStream;
 use proc_macro2::Span;
